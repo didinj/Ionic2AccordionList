b98d3cb27ab634ca059ae270b579db8b3809d8c6
didin jamaludin <didinj@gmail.com>
2017-03-19 20:02:47 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ionic2AccordionList
+Find full tutorial in [How to Create Ionic 2 Accordion List](https://www.djamware.com/post/5892739480aca7411808fa9c/how-to-create-ionic-2-accordion-list).
+
+To run locally:
+
+* Clone this repo
+* Run 'npm install'
+* Run 'ionic serve -l'
---
+++ src/pages/home/home.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  diseases = [
+    { title: "Type 1 Diabetes", description: "Type 1 diabetes is an autoimmune disease in which the body’s immune system attacks and destroys the beta cells in the pancreas that make insulin." },
+    { title: "Multiple Sclerosis", description: "Multiple sclerosis (MS) is an autoimmune disease in which the body's immune system mistakenly attacks myelin, the fatty substance that surrounds and protects the nerve fibers in the central nervous system." },
+    { title: "Crohn's & Colitis", description: "Crohn's disease and ulcerative colitis (UC), both also known as inflammatory bowel diseases (IBD), are autoimmune diseases in which the body's immune system attacks the intestines." },
+    { title: "Lupus", description: "Systemic lupus erythematosus (lupus) is a chronic, systemic autoimmune disease which can damage any part of the body, including the heart, joints, skin, lungs, blood vessels, liver, kidneys and nervous system." },
+    { title: "Rheumatoid Arthritis", description: "Rheumatoid arthritis (RA) is an autoimmune disease in which the body's immune system mistakenly begins to attack its own tissues, primarily the synovium, the membrane that lines the joints." }
+  ];
+
+  shownGroup = null;
+
+  constructor(public navCtrl: NavController) {
+  }
+
+  toggleGroup(group) {
+      if (this.isGroupShown(group)) {
+          this.shownGroup = null;
+      } else {
+          this.shownGroup = group;
+      }
+  };
+  isGroupShown(group) {
+      return this.shownGroup === group;
+  };
+
+}
